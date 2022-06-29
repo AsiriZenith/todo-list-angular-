5ccf357c8a0ee44b9f10207b119b417d8b9430ca
khguruge <gkasunharshana@gmail.com>
2022-06-29 20:00:22 +0530

    login-done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  loginForm: FormGroup;
+  username_error = false;
+  password_error = false;
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
   }
 
+  ngOnInit(): void { }
+
+  onSubmit() {
+    console.log(this.loginForm.value.username);
+    console.log(this.loginForm.value.password);
+    if(this.loginForm.value.username == 'admin' && this.loginForm.value.password == 'admin'){
+      alert("hello")
+    }
+    if(this.loginForm.value.username != 'admin'){
+      this.username_error = true;
+    }
+    if(this.loginForm.value.password != 'admin'){
+      this.password_error = true;
+    }
+
+  }
+
+  getFormControl(name: any) {
+    return this.loginForm.controls[name];
+  }
 }
